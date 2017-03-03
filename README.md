b40ecf01edcba81428773c66e861e5ba74e3d08e
Ceyhun ÖZGÜN <ceyhun.ozgun@cs.com.tr>
2017-03-03 23:24:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"Card Store Application" 
---
+++ src/main/java/com/cardstore/CardstoreApplication.java
@@ -0,0 +1,12 @@
+package com.cardstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardstoreApplication.class, args);
+	}
+}
